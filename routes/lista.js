9023cd82e9339a4a22ef6c56c3fa8b69a1d3c1dc
Thomaz <priamothomaz@gmail.com>
2026-01-28 21:42:14 -0300

    a

--- routes/lista.js
+++ routes/lista.js
@@ -5,24 +5,24 @@ const verificarToken = require("../middlewares/authToken.js");
 const router = express.Router();
 
 router.get("/", verificarToken, async (req, res) => {
-    const { limit, offset, busca} = req.params
+    const { limit, offset, busca } = req.params
     try {
         let limitSql = parseInt(limit) || 15
         let offsetSql = parseInt(offset) || 0
 
-        if (busca){
+        if (busca) {
             const result = await pool.query(`SELECT catalogo.usuario_id as catalogo_usuario, catalogo.nome as produto_nome, catalogo.categoria as produto_categoria, lista.id as lista_id, lista.usuario_id as lista_usuario_id, lista.quantidade as quantidade, lista.status as status, lista.data_atribuicao, lista.data_compra  FROM public.lista
         JOIN public.catalogo on catalogo.id = lista.produto_id
         WHERE catalogo.usuario_id = $1 AND catalogo.nome = $2
-        ORDER BY produto_categoria ASC, produto_nome ASC  LIMIT $3 OFFSET $4`, [req.user.id, busca, limitSql, offsetSql])   
-        return res.status(200).json(result.rows)  
+        ORDER BY produto_categoria ASC, produto_nome ASC  LIMIT $3 OFFSET $4`, [req.user.id, busca, limitSql, offsetSql])
+            return res.status(200).json(result.rows)
         }
 
         const result = await pool.query(`SELECT catalogo.usuario_id as catalogo_usuario, catalogo.nome as produto_nome, catalogo.categoria as produto_categoria, lista.id as lista_id, lista.usuario_id as lista_usuario_id, lista.quantidade as quantidade, lista.status as status, lista.data_atribuicao, lista.data_compra  FROM public.lista
         JOIN public.catalogo on catalogo.id = lista.produto_id
         WHERE catalogo.usuario_id = $1
         ORDER BY produto_categoria ASC, produto_nome ASC  LIMIT $2 OFFSET $3`, [req.user.id, limitSql, offsetSql])
-        
+
         res.status(200).json(result.rows)
     }
     catch (erro) {
@@ -31,5 +31,57 @@ router.get("/", verificarToken, async (req, res) => {
             detalhes: erro.message
         })
     }
-    
+
 })
+router.post("/", verificarToken, async (req, res) => {
+    const { produto_id, quantidade } = req.body
+    const status = "pendente"
+    let data_atribuicao = new Date()
+    if (!produto_id || !quantidade) {
+        return res.status(400).json({ erro: "É necessário preencher todas as informações" })
+    }
+    try {
+        const result = await pool.query(`INSERT INTO public.lista(usuario_id, produto_id, quantidade, status, data_atribuicao)
+	    VALUES ($1, $2, $3, $4, $5) returning *;`, [req.user.id, produto_id, quantidade, status, data_atribuicao])
+        res.status(200).json(result.rows[0])
+    }
+    catch (erro) {
+        res.status(500).json({
+            error: "Não foi possivel adicionar o produto a lista",
+            detalhes: erro.message
+        })
+    }
+
+})
+router.put("/:id", verificarToken, async(req,res)=>{
+    const {produto_id, quantidade, status, data_compra} = req.body
+    if (!produto_id || !quantidade || !status || !data_compra ){
+        return res.status(400).json({erro: "É necessário preencher todas as informações"})
+    }
+    try {
+        const result = await pool.query(`UPDATE public.lista
+	SET produto_id=$1, quantidade=$2, status=$3, data_compra=$4
+	WHERE id = $5 and usuario_id = $6 returning *`,[produto_id, quantidade, status, data_compra, req.params.id, req.user.id])
+    return res.status(200).json(result.rows[0])
+    }
+    catch(erro){
+        res.status(500).json({
+            error: "Não foi possivel atualizar o produto selecionado",
+            detalhes: erro.message
+        })
+    }
+})
+router.delete("/:id", verificarToken, async(req,res)=>{
+    try{
+        const result = await pool.query(`DELETE FROM public.lista
+	WHERE id = $1 and usuario_id = $2 returning *`,[req.params.id, req.user.id])
+    return res.status(200).json(result.rows[0])
+    }
+    catch(erro){
+        res.status(500).json({
+            error:"Não foi possivel deletar o produto selecionado",
+            detalhes: erro.message
+        })
+    }
+})
+module.exports = router
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const usuarioRouter = require("./routes/usuario.js")
 const catalogoRouter = require("./routes/catalogo.js")
+const listaRouter = require("./routes/lista.js")
 
 const app = express()
 app.use(cors())
@@ -11,6 +12,7 @@ app.use(express.json())
 
 app.use("/usuario", usuarioRouter)
 app.use("/catalogo", catalogoRouter)
+app.use("/lista", listaRouter)
 
 const PORT = process.env.PORT || 3000;
 
